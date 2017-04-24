d250fd015420f0ca45a6ebab12ce194fd3f31b82
Ian Denhardt <ian@zenhack.net>
2017-04-24 18:51:39 -0400

    Strip executable
    ...moving the debug info to another file. This reduces spk size
    somewhat.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 set -ex
 cd "$(dirname $0)"
-go build -v -i
+go build -v -i -o app
+strip app -o app.stripped
+objcopy --only-keep-debug app app.debug
+objcopy --add-gnu-debuglink=app.debug app.stripped
 uglifyjs static/embed.js -m > static/embed.min.js
---
+++ clean.sh
@@ -0,0 +1,2 @@
+cd $(dirname $0)
+rm -f app app.stripped app.debug static/*.min.js
